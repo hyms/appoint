2d8c0cf6fdba022913a0bb3428a9a188f6970d8a
helier <hdnymib@gmail.com>
2025-05-15 21:36:23 -0400

    reset proyect init only necesary files

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'appoint',
-  webDir: 'dist'
-};
-
-export default config;
